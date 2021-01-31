a521fb855884bd029a505a0ff46a5db26ef6e7cc
lpariza.13 <lpariza.13@est.ucab.edu.ve>
2021-01-31 18:33:32 -0400

    Segunda version modelo de base de datos

--- backend/main/admin.py
+++ backend/main/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from .models import Sandwich, Ingrediente, Sand_Ing, Pedido, Ped_Sand
+from .models import Sandwich, Ingrediente, Sand_Ing, Pedido
 
 admin.site.register(Sandwich)
 admin.site.register(Ingrediente)
 admin.site.register(Sand_Ing)
 admin.site.register(Pedido)
-admin.site.register(Ped_Sand)
+
 
 
 # Register your models here.
