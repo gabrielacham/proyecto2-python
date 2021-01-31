161b5e514ee652393a349f36f872cc8357e4e223
gregorybustamante <gsbustamante.14@est.ucab.edu.ve>
2021-01-31 17:29:27 -0400

    se modifico el modulo de los serializadores

--- backend/main/api/serializers.py
+++ backend/main/api/serializers.py
@@ -6,31 +6,31 @@ from main.models import Ingrediente,Sandwich,Sand_Ing,Pedido,Ped_Sand
 class IngredienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ingrediente
-        fields = ['id', 'question_text', 'pub_date']
+        
         fields = '__all__'
 
 class SandwichSerializer(serializers.ModelSerializer):
     class Meta:
         model = Sandwich
-        # fields = ['id', 'question_text', 'pub_date']
+        
         fields = '__all__'
 
 
 class Sand_IngSerializer(serializers.ModelSerializer):
     class Meta:
         model = Sand_Ing
-        # fields = ['id', 'question_text', 'pub_date']
+        
         fields = '__all__'
 
 
 class PedidoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Pedido
-        # fields = ['id', 'question_text', 'pub_date']
+        
         fields = '__all__'
 
 class Ped_SandSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ped_Sand
-        # fields = ['id', 'question_text', 'pub_date']
+        
         fields = '__all__'
