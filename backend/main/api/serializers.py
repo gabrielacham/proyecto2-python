13d9973ff64ae451b30c7c237734e6e9b2ad4b47
lpariza.13 <lpariza.13@est.ucab.edu.ve>
2021-02-01 00:00:34 -0400

    Se modifico el modulo serializers para que haga post

--- backend/main/api/serializers.py
+++ backend/main/api/serializers.py
@@ -1,20 +1,19 @@
 from rest_framework import serializers
 from main.models import Ingrediente, Sandwich, Pedido
 
-class IngredienteSerializer(serializers.Serializer):
-    nombre_ingrediente  = serializers.CharField(max_length=200)
-    precio_ingrediente = serializers.DecimalField(max_digits=8,decimal_places=2, default=0)
+class IngredienteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ingrediente
+        fields = '__all__'
 
-class SandwichSerializer(serializers.Serializer):
-    tamano_sandwich = serializers.CharField(max_length=50)
-    precio_sandwich = serializers.DecimalField(max_digits=8,decimal_places=2, default=0)
-    ingredientes= IngredienteSerializer(read_only=True, many=True)
+class SandwichSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Sandwich
+        fields = '__all__'
 
-class PedidoSerializer(serializers.Serializer):
-    descrip_pedido = serializers.CharField(max_length=200)
-    precio_pedido = serializers.DecimalField(max_digits=8,decimal_places=2, default=0)
-    fecha_pedido  = serializers.DateTimeField('Fecha del Pedido')
-    porcentaje_oferta= serializers.DecimalField(max_digits=8,decimal_places=2, default=0)
-    sandwichs= SandwichSerializer(read_only=True, many=True)
+class PedidoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pedido
+        fields = '__all__'
 
 
