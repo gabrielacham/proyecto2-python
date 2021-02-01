471d4e975d783cba9c022ab155f1395c01a85c9b
lpariza.13 <lpariza.13@est.ucab.edu.ve>
2021-01-31 20:02:08 -0400

    Prueba del modulo models usando manytomany

--- backend/main/admin.py
+++ backend/main/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Sandwich, Ingrediente, Sand_Ing, Pedido
+from .models import Sandwich, Ingrediente, Pedido#,Sand_Ing
 
 admin.site.register(Sandwich)
 admin.site.register(Ingrediente)
-admin.site.register(Sand_Ing)
+#admin.site.register(Sand_Ing)
 admin.site.register(Pedido)
 
 
--- backend/main/models.py
+++ backend/main/models.py
@@ -4,7 +4,7 @@ import datetime
 class Pedido(models.Model):
     descrip_pedido = models.CharField(max_length=200)
     precio_pedido = models.DecimalField(max_digits=8,decimal_places=2, default=0)
-    fecha_pedido = models.DateTimeField('fecha del Pedido')
+    fecha_pedido = models.DateTimeField('Fecha del Pedido')
     porcentaje_oferta = models.DecimalField(max_digits=8,decimal_places=2, default=0)
     
     def __str__(self):
@@ -17,7 +17,7 @@ class Pedido(models.Model):
 class Ingrediente(models.Model):
     nombre_ingrediente = models.CharField(max_length=200)
     precio_ingrediente = models.DecimalField(max_digits=8,decimal_places=2, default=0)
-
+    
     def __str__(self):
         return self.nombre_ingrediente
 
@@ -27,12 +27,13 @@ class Sandwich(models.Model):
     tamano_sandwich = models.CharField(max_length=200)
     precio_sandwich = models.DecimalField(max_digits=8,decimal_places=2, default=0)
     pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
-    
+    ingrediente = models.ManyToManyField(Ingrediente)
+
     def __str__(self):
         return self.tamano_sandwich
 
 
-class Sand_Ing(models.Model):
-    tipo_sandwich = models.ForeignKey(Sandwich, on_delete=models.CASCADE)
-    ingrediente = models.ForeignKey(Ingrediente, on_delete=models.CASCADE)
+#class Sand_Ing(models.Model):
+ #   tipo_sandwich = models.ForeignKey(Sandwich, on_delete=models.CASCADE)
+  #  ingrediente = models.ForeignKey(Ingrediente, on_delete=models.CASCADE)
 
