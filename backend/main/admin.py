109617752e6a21c95113df803d88177517729a72
lpariza.13 <lpariza.13@est.ucab.edu.ve>
2021-02-02 03:52:17 -0400

    Primera version del sitio administrativo con reportes de ventas

--- backend/main/admin.py
+++ backend/main/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
 from .models import Sandwich, Ingrediente, Pedido#,Sand_Ing
 
-admin.site.register(Sandwich)
-admin.site.register(Ingrediente)
-#admin.site.register(Sand_Ing)
-admin.site.register(Pedido)
+class SandwichInline(admin.TabularInline):
+    model = Sandwich
+    
+
+class PedidoAdmin(admin.ModelAdmin):
+     fieldsets = [
+    (None, {'fields': ['descrip_pedido','fecha_pedido','precio_pedido','porcentaje_oferta']}),
+    ]
+     inlines = [SandwichInline]
+     list_display = ('descrip_pedido','fecha_pedido')
+     list_filter = ['fecha_pedido']
+     search_fields = ['descrip_pedido']
+
+admin.site.register(Pedido,PedidoAdmin)
 
 
-
-# Register your models here.
