4c871cfdf53e7ea0c250c1da227256190bd2ef91
Gabriela <gabrielachiquinquira01@gmail.com>
2021-02-01 22:21:02 -0400

    Add create order and get ingredients functionality

--- frontend/src/views/Home/index.js
+++ frontend/src/views/Home/index.js
@@ -8,8 +8,10 @@ import {
   FormGroup,
   Label,
   CustomInput,
+  Input,
   Button,
   Modal,
+  ModalHeader,
   ModalBody,
   ModalFooter
  } from 'reactstrap';
@@ -32,37 +34,6 @@ const sandwichSizes = [
   }
 ];
 
-const ingredients = [
-  {
-    nombre_in: 'Jamon',
-    precio_in: 40
-  },
-  {
-    nombre_in: 'Champiñones',
-    precio_in: 35
-  },
-  {
-    nombre_in: 'Pimentón',
-    precio_in: 30
-  },
-  {
-    nombre_in: 'Doble queso',
-    precio_in: 40
-  },
-  {
-    nombre_in: 'Aceitunas',
-    precio_in: 57.5
-  },
-  {
-    nombre_in: 'Pepperoni',
-    precio_in: 38.5
-  },
-  {
-    nombre_in: 'Salchichón',
-    precio_in: 62.5
-  },
-];
-
 
 export default function Home(props) {
 
@@ -71,6 +42,9 @@ export default function Home(props) {
   const [subtotal, setSubtotal] = useState(0);
   const [total, setTotal] = useState(0);
   const [order, setOrder] = useState([]);
+  const [idpedido, setPedidoId] = useState(0);
+  const [ingredients, setIngredients] = useState([]);
+  const [descripcion, setDescripcion] = useState('');
   const [sandwich, setSandwich] = useState(sandwichSizes[0] ? sandwichSizes[0].tamano_san : 0);
   const prevSandwich = usePrevious(sandwich);
 
@@ -82,6 +56,7 @@ export default function Home(props) {
   return ref.current;
 }
 
+  // ComponentDidUpdate
   useEffect(() => {
     if(prevSandwich !== sandwich) {
       let aux
@@ -97,16 +72,40 @@ export default function Home(props) {
     }
   }, [prevSandwich, sandwich, subtotal])
 
+  // ComponentDidMount
+  useEffect(() => {
+    axios
+      .get("http://127.0.0.1:8000/main/api/Ingrediente/")
+      .then(res => setIngredients(res.data))
+      .catch(err => console.log(err));
+}, [setIngredients]);
+
   const toggle = () => setModal(!modal);
 
-  const toggleInitModal = () => setInitModal(!initModal);
+  const toggleInitModal = () => {
+    var currentdate = new Date();
+    let aux ={
+      id:0,
+      porcentaje_oferta: 0,
+      descrip_pedido: descripcion,
+      precio_pedido: 0,
+      fecha_pedido: currentdate.toISOString()
+    }
+    axios
+      .post("http://127.0.0.1:8000/main/api/Pedido/", aux)
+      .then(res => setPedidoId(res.data.id))
+      .catch(err => console.log(err));
+    setInitModal(!initModal)
+  };
 
 
   function renderOptions (data) {
     return (
         data.map((prop, key) => {
             return (
-              <option key={key}>{prop.tamano_san}</option>
+              <option key={key}>
+                {prop.tamano_san}
+              </option>
             );
         })
     );
@@ -114,35 +113,50 @@ export default function Home(props) {
 
   function renderIngredients (data) {
     return (
+      data ?
         data.map((prop, key) => {
             return (
-              <Row key={key} className='justify-content-between ml-1 px-3'>
-                <CustomInput type="checkbox" id={`ingredient${key}`} label={prop.nombre_in} onChange={() => updateCheckSubtotal(prop)}/>
-                <Label>{`${prop.precio_in} Bs`}</Label>
+              <Row
+                key={key}
+                className='justify-content-between ml-1 px-3'
+              >
+                <CustomInput
+                  type="checkbox"
+                  id={`ingredient${key}`}
+                  label={prop.nombre_ingrediente}
+                  onChange={() => updateCheckSubtotal(prop)}
+                />
+                <Label>
+                  {`${prop.precio_ingrediente} Bs`}
+                </Label>
               </Row>
             );
-        })
+        }) : <div/>
     );
   }
 
-function renderBillIngredients(list){
-  if (list) {
-    return (
-      list.map((el, key) => {
-        return (
-          <Col key={key}>
-            <Row className='justify-content-between ml-4 pl-3'>
-              <Label>{`${el.nombre_in} Bs`}</Label>
-                {' '}
-              <Label>{`${el.precio_in} Bs`}</Label>
-            </Row>
-          </Col>
-        );
-      })
-    );
+  function renderBillIngredients(list){
+    if (list) {
+      return (
+        list.map((el, key) => {
+          return (
+            <Col key={key}>
+              <Row className='justify-content-between ml-4 pl-3'>
+                <Label>
+                  {`${el.nombre_ingrediente} Bs`}
+                </Label>
+                  {' '}
+                <Label>
+                  {`${el.precio_ingrediente} Bs`}
+                </Label>
+              </Row>
+            </Col>
+          );
+        })
+      );
+    }
+    return;
   }
-  return;
-}
 
   function renderBill (){
     if (order){
@@ -151,9 +165,13 @@ function renderBillIngredients(list){
           return (
             <Col key={key}>
               <Row className='justify-content-between ml-2 px-3'>
-                <Label>{`${el.tamano_san}`}</Label>
+                <Label>
+                  {`${el.tamano_san}`}
+                </Label>
                   {' '}
-                <Label>{`${el.precio_san} Bs`}</Label>
+                <Label>
+                  {`${el.precio_san} Bs`}
+                </Label>
               </Row>
               {renderBillIngredients(el.ingredients)}
             </Col>
@@ -168,10 +186,10 @@ function renderBillIngredients(list){
     prop.checked=!prop.checked;
     let aux
     if (prop.checked) {
-      aux= subtotal+prop.precio_in
+      aux= subtotal+prop.precio_ingrediente
       setSubtotal(aux)
     } else {
-      aux= subtotal-prop.precio_in
+      aux= subtotal-prop.precio_ingrediente
       setSubtotal(aux)
     }
   }
@@ -197,38 +215,74 @@ function renderBillIngredients(list){
         <Row>
 
           {/* Init Modal */}
-          <Modal isOpen={initModal} toggle={toggleInitModal}>
-            <ModalBody className='d-flex justify-content-center'>
+          <Modal
+            isOpen={initModal}
+            toggle={toggleInitModal}
+          >
+            <ModalHeader className='d-flex justify-content-center'>
               <Col>
                 <Row className='justify-content-center h4'>
                   ¡Bienvenido a Sandwiches UCAB!
                 </Row>
                 <Row className='justify-content-center'>
-                  <img src={logo} style={{width: '4.3rem'}} alt=''/>
+                  <img
+                    src={logo}
+                    style={{width: '4.3rem'}}
+                    alt=''
+                  />
                 </Row>
               </Col>
+            </ModalHeader>
+            <ModalBody className='d-flex justify-content-center'>
+              <FormGroup>
+                <Label for="descripcion">
+                  Describa su Pedido
+                </Label>
+                <Input
+                  type="textarea"
+                  name="descripcion"
+                  id="descripcion"
+                  onChange={({ target }) => setDescripcion(target.value)}
+                />
+              </FormGroup>
             </ModalBody>
             <ModalFooter className='d-flex justify-content-center'>
-              <Button  onClick={toggleInitModal}>Iniciar la Compra</Button>
+              <Button
+                onClick={toggleInitModal}
+              >
+                Iniciar la Compra
+              </Button>
             </ModalFooter>
           </Modal>
 
           {/* Order Column */}
           <Col sm='7'>
             <Card className='p-3'>
-              <CardTitle className='h4'>Ingrese su Orden</CardTitle>
+              <CardTitle className='h4'>
+                Ingrese su Orden
+              </CardTitle>
 
               {/* Size Select */}
               <FormGroup>
-                <Label for="sandwichSize">Tamaño</Label>
-                <CustomInput className='pr-3' type="select" name="sandwichSize" id="sandwichSize" onChange={({ target }) => setSandwich(target.value)}>
+                <Label for="sandwichSize">
+                  Tamaño
+                </Label>
+                <CustomInput
+                  className='pr-3'
+                  type="select"
+                  name="sandwichSize"
+                  id="sandwichSize"
+                  onChange={({ target }) => setSandwich(target.value)}
+                >
                   {renderOptions(sandwichSizes)}
                 </CustomInput>
               </FormGroup>
 
               {/* Additional Ingredients */}
               <FormGroup>
-                <Label for="adicionales">Ingredientes Adicionales</Label>
+                <Label for="adicionales">
+                  Ingredientes Adicionales
+                </Label>
                 <div>
                   {renderIngredients(ingredients)}
                 </div>
@@ -238,20 +292,34 @@ function renderBillIngredients(list){
 
               {/*Subtotal Row*/}
               <Row className='my-2 ml-2 mr-0 justify-content-between'>
-                <Label for="subtotal">Subtotal</Label>
-                <Label for="subtotal">{subtotal} Bs</Label>
+                <Label for="subtotal">
+                  Subtotal
+                </Label>
+                <Label for="subtotal">
+                  {subtotal} Bs
+                </Label>
               </Row>
 
               {/* Buttons Row */}
               <Row className='mx-1 mt-2'>
                 {/* Cancel Button */}
                 <Col sm='6'>
-                  <Button outline className='home-cancel-button'>Cancelar</Button>
+                  <Button
+                    outline
+                    className='home-cancel-button'
+                  >
+                    Cancelar
+                  </Button>
                 </Col>
 
                 {/* Accept Button */}
                 <Col sm='6' className='d-flex justify-content-end'>
-                  <Button onClick={toggle} className='home-button'>Aceptar</Button>
+                  <Button
+                    onClick={toggle}
+                    className='home-button'
+                  >
+                    Aceptar
+                  </Button>
                 </Col>
               </Row>
             </Card>
@@ -263,31 +331,49 @@ function renderBillIngredients(list){
               ¿Desea agregar el sandwich a su pedido?
             </ModalBody>
             <ModalFooter className='d-flex justify-content-between'>
-              <Button outline className='home-cancel-button' onClick={toggle}>Cancelar</Button>
+              <Button
+                outline
+                className='home-cancel-button'
+                onClick={toggle}
+              >
+                Cancelar
+              </Button>
                 {' '}
-              <Button  onClick={() => addSandwich()}>Continuar</Button>
+              <Button
+                onClick={() => addSandwich()}
+              >
+                Continuar
+              </Button>
             </ModalFooter>
           </Modal>
 
           {/* Bill Column */}
           <Col sm='5'>
             <Card className='p-3'>
-              <CardTitle className='h4'>Factura</CardTitle>
+              <CardTitle className='h4'>
+                Factura
+              </CardTitle>
               {renderBill()}
 
               <hr className="mb-2 mt-0"/>
 
               {/*Subtotal Row*/}
               <Row className='my-2 ml-2 mr-0 justify-content-between'>
-                <Label for="subtotal">Total</Label>
-                <Label for="subtotal">{total} Bs</Label>
+                <Label for="subtotal">
+                  Total
+                </Label>
+                <Label for="subtotal">
+                  {total} Bs
+                </Label>
               </Row>
 
               <hr className="mb-2 mt-0"/>
 
               {/* Buttons Row */}
               <Row className='mx-1 mt-2'>
-                <Button className='justify-content-center home-finish-button'>Finalizar Pedido</Button>
+                <Button className='justify-content-center home-finish-button'>
+                  Finalizar Pedido
+                </Button>
               </Row>
             </Card>
           </Col>
