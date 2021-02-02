a7d1bd72276e75db66a0549bcd651476da5b2662
bryrodri <bryrodri1@gmail.com>
2021-02-02 10:09:58 -0800

    admin primera parte

--- backend/main/admin.py
+++ backend/main/admin.py
@@ -4,8 +4,21 @@ from .models import Sandwich, Ingrediente, Pedido#,Sand_Ing
 admin.site.register(Sandwich)
 admin.site.register(Ingrediente)
 #admin.site.register(Sand_Ing)
-admin.site.register(Pedido)
+
+class SandwichInline(admin.StackedInline):
+    model=Sandwich
+    extra = 0
 
 
+
+class PedidoAdmin(admin.ModelAdmin):
+    fieldsets = [
+    ("informacion del pedido", {'fields': ['fecha_pedido','descrip_pedido']}),
+    ('Informacion de pago', {'fields': ['precio_pedido','porcentaje_oferta']}),]
+    readonly_fields=['precio_pedido','porcentaje_oferta','fecha_pedido','descrip_pedido']
+    inlines = [SandwichInline]
+
+
+admin.site.register(Pedido, PedidoAdmin)
 
 # Register your models here.
